b2a42eabf9b3d33bfd6afd3202be7f89f57bd9f9
Sagar naik <sagarnaikg00@gmail.com>
2021-07-20 18:01:25 +0530

    base project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Home from "./pages/home";
+import "./styles/style.scss";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import designImage from "../assets/design.png";
+import { gsap, Power2 } from "gsap";
+
+const Home = () => {
+  useEffect(() => {
+    let tl = gsap.timeline();
+    tl.to(".container", 0.4, { visibility: "visible" })
+      .to(".img-cover", 1.4, {
+        width: "0%",
+        ease: Power2.easeInOut,
+      })
+      .from(".image", 1.4, {
+        scale: 1.4,
+        ease: Power2.easeInOut,
+        delay: -1.4,
+      });
+  }, []);
+
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="img-container">
+          <div className="img-cover"></div>
+          <img className="image" src={designImage} alt="design" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
